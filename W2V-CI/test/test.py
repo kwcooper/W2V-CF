72d03b4fa4ecba3d43636273f45b4255a65ee4c8
Keiland <officialkwade@gmail.com>
2018-05-13 01:43:55 -0400

    Ud

---
+++ W2V-CI/test/test.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+##print('hi')
+##v = {'a':[1,2,3], 'b':[4,5,6]}
+##
+##df = pd.DataFrame(v)
+##
+##df.to_csv('out.csv')
+##
+##print('fin')
+##
+##with open()
+
+
+mat = np.zeros
+
+
